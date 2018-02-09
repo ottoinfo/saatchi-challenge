70a846db03539c1b0fb7107f8ac05de9a1a37776
Matt Otto <matt@ottoinfo.com>
2018-02-09 15:49:06 -0800

    Fix File Name and Excess Code

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -1,8 +1,8 @@
 import { connect } from "react-redux"
 
-import ArtworksList from "../components/Search"
+import Search from "../components/Search"
 
 const mapStateToProps = state => ({})
 const mapDispatchToProps = dispatch => ({})
 
-export default connect(mapStateToProps, mapDispatchToProps)(ArtworksList)
+export default connect(mapStateToProps, mapDispatchToProps)(Search)
